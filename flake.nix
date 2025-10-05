ae4a0b61d63ddf1fea9549545f06220d6daa5757
Christian Georgii <cgeorgii@gmail.com>
2025-10-05 17:39:52 +0200

    Nixify project

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Eve - An extensible event framework";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+
+        haskellPackages = pkgs.haskellPackages;
+
+        eve = haskellPackages.callCabal2nix "eve" ./. {};
+
+      in {
+        packages = {
+          default = eve;
+          eve = eve;
+        };
+
+        devShells.default = haskellPackages.shellFor {
+          packages = p: [ eve ];
+          buildInputs = with haskellPackages; [
+            cabal-install
+            haskell-language-server
+          ];
+          withHoogle = true;
+        };
+      }
+    );
+}
